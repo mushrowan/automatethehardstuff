a7363408697af156d0ca73da0371984628caef5e
rowan amber jones <rowamber@outlook.com>
2022-01-25 11:35:04 +0000

    Figured out how to check whether the keys in the input dictionary are all valid squares

--- chess_dictionary_validator.py
+++ chess_dictionary_validator.py
@@ -2,7 +2,7 @@
 
 sample_working_board = {'a2': 'bking', 'a3': 'wpawn', 'f4': 'wking'}
 sample_broken_board1 = {'a2': 'bking'} # Only has one king
-sample_broken_board2 = {'a9': 'bking', 'e4': 'wking'}
+sample_broken_board2 = {'a9': 'bking', 'e4': 'wking'} # 'a9' isn't a board square
 
 
 
@@ -10,5 +10,11 @@ sample_broken_board2 = {'a9': 'bking', 'e4': 'wking'}
 default_squares = [] 
 for letter in ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']:
     default_squares += [letter + str(int) for int in range(1, 9)]
+# Check if all dictionary keys (squares) are in the list of default squares
+print(list(sample_working_board.keys()))
+if set(sample_working_board.keys()).issubset(default_squares):
+    print('All keys are valid.')
+else: 
+    print('This ain\'t good, chief.')
 print(default_squares)
 
