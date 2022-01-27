6669b23591947f135a28b0e546edc9ad39448334
rowan amber jones <rowamber@outlook.com>
2022-01-27 21:13:19 +0000

    Did some more stuff. a few more chapters, and started work on the mclip program

---
+++ fstrings.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+name = 'Al'
+
+age = 4000
+
+print('My name is %s. I am %s years old.' % (name, age)) # String interpolation
+
+print(f'My name is {name}. Next year I will be {4000}.')
