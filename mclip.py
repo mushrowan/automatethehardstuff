8426a1e5984a88fd45fa7e67826329a64d9e984b
rowan amber jones <rowamber@outlook.com>
2022-01-28 11:40:52 +0000

    Pig latin added. why? nobody knows.

--- mclip.py
+++ mclip.py
@@ -4,3 +4,17 @@
 TEXT = {'agree': '''Yes, I agree. That sounds fun to me.''',
         'busy': '''Sorry, can we do this later this week or next week?''',
         'upsell': '''Would you consider making this a monthly donation?'''}
+
+import sys, pyperclip
+
+if len(sys.argv) < 2:
+    print('Usage: python mclip.py [keyphrase] - copy phrase text')
+    sys.exit()
+
+keyphrase = sys.argv[1] # first command arg is the keyphrase
+
+if keyphrase in TEXT:
+    pyperclip.copy(TEXT[keyphrase])
+    print('Text for ' + keyphrase + ' copied to clipboard.')
+else:
+    print('There is no text for ' + keyphrase)
