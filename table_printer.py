47fa68e21f99522a1ca45db0215d43ec117e21d7
rowan amber jones <rowamber@outlook.com>
2022-01-28 11:53:32 +0000

    Started work on the table_printer.py

---
+++ table_printer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# Takes a list of lists of strings and displays them in a well-organised table with each column right-justified. Assumes that each inner list contains the same number of strings.
+
+table_data = [['apples', 'oranges', 'cherries', 'banana'],
+        ['Alice', 'Bob', 'Carol', 'David'],
+        ['Dogs', 'cats', 'moose', 'goose']]
+
+
