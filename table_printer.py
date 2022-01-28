7ec28f888d8cb43245a06cf395f6f4870cfa1cf2
rowan amber jones <rowamber@outlook.com>
2022-01-28 23:02:57 +0000

    OMG THE TABLE PRINTER WORKED WITHOUT ANY DEBUGGING AT ALL YESSSS

--- table_printer.py
+++ table_printer.py
@@ -6,3 +6,26 @@ table_data = [['apples', 'oranges', 'cherries', 'banana'],
         ['Dogs', 'cats', 'moose', 'goose']]
 
 
+# Figure out the longest item in a string
+
+def calculate_column_width(input_list):
+    current_longest = 0
+    
+    for item in input_list:
+        if len(item) > current_longest:
+            current_longest = len(item)
+    current_longest += 1 # Add one so that we always have at least 1 space
+    return current_longest
+
+sub_list_lengths = [] # This list will consist of the length of the longest strings in each list.
+# sub_list_lengths[0] will be the longest length in table_data[0]. 
+
+for sub_list in table_data:
+    sub_list_lengths.append(calculate_column_width(sub_list))
+print(sub_list_lengths)
+    
+for item in range(len(table_data[0])): # We assume that the length of the first column will be the same as the length of all of the columns
+    row_string = ''
+    for list_no in range(len(table_data)): # Compute each column once at a time. list_no is the index of the column. we want, e.g. 'apples alice dogs'
+       row_string = row_string + table_data[list_no][item].rjust(sub_list_lengths[list_no])
+    print(row_string)
