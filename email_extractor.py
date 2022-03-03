e50c1a65d1d0613842ca711b9c6e631068a66dd9
Rowan Amber-Jones <rowamber@outlook.com>
2022-03-03 16:24:07 +0000

    email extractor has been developed

--- email_extractor.py
+++ email_extractor.py
@@ -14,12 +14,31 @@ import pyperclip, re
 input_string = str(pyperclip.paste())
 # TODO: Create an email address regex
 email_regex = re.compile(r'''(
-(\w+(\.\w+)*)       # Captures ghjk, fsdfa.adsfad, but not .asdfads., asdf..adfdsaf
-(@)                 # Obvious @ is obvious
-(\w+(\.\w+)+)       # Captures asd.adsf but not asdfad
-)
-''', re.VERBOSE)
+
+    (\w+(\.\w+)*)       # Captures ghjk, fsdfa.adsfad, but not .asdfads., asdf..adfdsaf
+    (@)                 # Obvious @ is obvious
+    (\w+(\.\w+)+)       # Captures asd.adsf but not asdfad
+
+)''', re.VERBOSE)
+
 # TODO: Create a phone number regex
+phone_number_regex = re.compile(r'''(
+
+    (0|\+440?|\+3530?)  # Capture the country code (probably there is an easier way to do this but we'll figure it out later)
+    (\d{9,10})          # Capture the rest of the number. For British numbers there are 9 or 10 digits to follow
+
+)''', re.VERBOSE)
 
 # TODO: Put text into the clipboard
+
+output_list = phone_number_regex.findall(input_string) + email_regex.findall(input_string)
+output_string = ''
+for match in output_list:
+    output_string += match[0]
+    output_string += '\n'
+
+
+
+
+pyperclip.copy(str(output_string))
 
