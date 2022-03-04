8d6da944151442399605e25284ce25c0381a4f74
Rowan Amber-Jones <rowamber@outlook.com>
2022-03-04 11:59:11 +0000

    made progress on password detection

--- strong_password_detection.py
+++ strong_password_detection.py
@@ -8,3 +8,14 @@ Strong password has:
 3. Has at least 1 digit.
 
 '''
+minimum_characters_regex = re.compile(r'''
+    \s
+    .*
+    .{8}
+    .*
+    \s
+''', re.VERBOSE)
+
+uppercase_and_lowercase_regex = re.compile(r'.*([a-z].*[A-Z]+|[A-Z].*[a-z]+).*')
+
+has_digit = re.compile(r'.*\d.*')
