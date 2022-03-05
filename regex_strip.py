3b8ff2473962772e4286470a099ef877a7abe45b
Rowan Amber-Jones <rowamber@outlook.com>
2022-03-05 20:50:18 +0000

    strong password detector finished. regex strip started.

---
+++ regex_strip.py
@@ -0,0 +1,4 @@
+import re
+'''Write a function that does the same as the strip() function.
+If no arguments are passed, whitespace characters should be removed.
+'''
--- strong_password_detection.py
+++ strong_password_detection.py
@@ -8,14 +8,21 @@ Strong password has:
 3. Has at least 1 digit.
 
 '''
-minimum_characters_regex = re.compile(r'''
-    \s
-    .*
-    .{8}
-    .*
-    \s
-''', re.VERBOSE)
+test_input = 'MattBellamy0!!'
+minimum_characters_regex = re.compile(r'.*.{8}.*')
 
 uppercase_and_lowercase_regex = re.compile(r'.*([a-z].*[A-Z]+|[A-Z].*[a-z]+).*')
 
 has_digit = re.compile(r'.*\d.*')
+
+
+is_secure = False
+if minimum_characters_regex.fullmatch(test_input):
+    print('Minimum characters reached!')
+    if uppercase_and_lowercase_regex.fullmatch(test_input):
+        print('Has uppercase and lowercase!')
+        if has_digit.fullmatch(test_input):
+            print('has at least one number in it!')
+            is_secure = True
+
+print(is_secure)
